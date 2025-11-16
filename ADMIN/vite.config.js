0684cb8154b618ab44ff90a4a16f6745cd9b16b6
headsilindhinsi <headslindhinsi@gmail.com>
2025-11-16 10:08:07 +0530

    commit

--- ADMIN/vite.config.js
+++ ADMIN/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig(({ command }) => {
       server: {
         host: "0.0.0.0",
         port: 5000,
-        allowedHosts: ["finalproject-8-39n0.onrender.com"]
+        allowedHosts: ["finalproject-9-o6hx.onrender.com"]
       }
     }
   }
