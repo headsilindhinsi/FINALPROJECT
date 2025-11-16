56766f59b42de8dd5980d1fa7c78a8e5ad731327
headsilindhinsi <headslindhinsi@gmail.com>
2025-11-16 09:44:50 +0530

    commit

--- ADMIN/vite.config.js
+++ ADMIN/vite.config.js
@@ -1,12 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server : {
-    host : "0.0.0.0",
-    port : 5000,
-    allowedHosts : ['finalproject-6-dwx9.onrender.com']
+export default defineConfig(({ command }) => {
+  
+  if (command === 'serve') {
+    // ✔ Only for local development
+    return {
+      plugins: [react()],
+      server: {
+        host: "0.0.0.0",
+        port: 5000,
+        allowedHosts: ["finalproject-6-dwx9.onrender.com"]
+      }
+    }
+  }
+
+  // ✔ Production build (Render uses this)
+  return {
+    plugins: [react()]
   }
 })
